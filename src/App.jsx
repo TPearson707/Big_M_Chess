f820fd92f8b1c8754d8ef3868f8344306a1b82cd
TPearson707 <pearson.e.thomas@gmail.com>
2025-01-29 16:43:46 -0500

    aaaa

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import './app.scss';
 
 import PrizeWinners from "./components/prizewinners/PrizeWinners";
+import Navbar from './components/navbar/Navbar';
 
 const App = () => {
   return (
     <div>
       <section className="Homepage">
+        <Navbar/>
         <PrizeWinners/>
       </section>
 
-      <section className="Banana">
-        Banana
-      </section>
+      <section className="Banana"></section>
       </div>
   )
 }
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import "./navbar.scss"
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="wrapper">
+                <span><a href="sa;lkjfd">Winter 2025</a></span>
+                <span><a href="askjfd">Prize Winners</a></span>
+                <span><a href="asklds">Pairings and Results</a></span>
+                <span><a href="a;lksjfd">Regulations</a></span>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
